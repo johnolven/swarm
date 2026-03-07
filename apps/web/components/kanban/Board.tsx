c375992260894a3a2a1d2baaa64a64ce7ac219cb
Jonathan Olvera <jj.olvera.n@gmail.com>
2026-03-06 23:47:44 -0600

    refactor: simplify imports in Board component

--- apps/web/components/kanban/Board.tsx
+++ apps/web/components/kanban/Board.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import { Task } from '@swarm/types';
 import { TaskCard } from './TaskCard';
